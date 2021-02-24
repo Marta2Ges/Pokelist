769f30651032fbd2d76e518ef983fc754df627b8
Marta2Ges <ggarridomarta@gmail.com>
2021-02-24 09:04:30 +0100

    Clone repository

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import data from "../data/data.json";
+import "../stylesheets/App.css";
+
+class App extends React.Component {
+  render() {
+    return <div>Hola mundo!</div>;
+  }
+}
+
+export default App;
